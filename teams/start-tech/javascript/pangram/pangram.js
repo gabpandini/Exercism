591bcd1244d2e4d11466de41f61bbd03c774f76b
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-08-27 19:31:44 -0300

    javascript exercicio 15 - pangram

--- teams/start-tech/javascript/pangram/pangram.js
+++ teams/start-tech/javascript/pangram/pangram.js
@@ -9,15 +9,7 @@ export const isPangram = (frase) => {
   //const fraseMinuscula = frase.toLowerCase();
   const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
   const chars = frase.toLowerCase().split("");
-  /*
-  for (let index = 0; index < frasePronta.length; index++) {
-  let resultado = {};
-  for (const frases of fraseMinuscula){
-    resultado[frases] !== alfabeto;
-
-  }
-  return true;
-  */
+  
 
   for (let letras of alfabeto) {
     if (!chars.includes(letras)) {
