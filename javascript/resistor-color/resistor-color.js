f9b5e2ffe932fbe00a03b62ad993c0affe384fa8
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-09-28 09:21:44 -0300

    js exercise 16 - resistor color

---
+++ javascript/resistor-color/resistor-color.js
@@ -0,0 +1,24 @@
+//
+// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const colorCode = (color) => {
+  return obj[color];
+};
+
+const obj = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9
+  };
+
+export const COLORS = Object.keys(obj);
+
