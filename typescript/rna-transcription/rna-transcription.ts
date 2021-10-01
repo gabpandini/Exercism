db547ee4bd5aa3a67d107080104dcfcb1d8bc53d
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-10-01 16:56:54 -0300

    ts exercise 5 - rna transcription

---
+++ typescript/rna-transcription/rna-transcription.ts
@@ -0,0 +1,19 @@
+type DnaBase = keyof typeof DnaBasis;
+
+enum DnaBasis {
+  G = 'C',
+  C = 'G',
+  T = 'A',
+  A = 'U',
+}
+
+export function toRna(DNA: string): string {
+ let resultado = '';
+  for (const elemento of DNA) {
+    if (!(elemento in DnaBasis))      {
+      throw new Error ('Invalid input DNA.')
+    }
+     resultado += DnaBasis[<DnaBase>elemento];
+  }
+  return resultado;
+}
