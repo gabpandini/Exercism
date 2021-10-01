a3ee24039c48524943261f6b90dab6e1d0db61b5
gabriela.pandini <gabriela.pandini@ambevtech.com.br>
2021-10-01 17:27:54 -0300

    ts exercise 6 - pangram

---
+++ typescript/pangram/pangram.ts
@@ -0,0 +1,5 @@
+export function isPangram(frase: string): boolean {
+  const filtrado = frase.toUpperCase().split('').filter(char => char >= 'A' && char <= 'Z');
+  const setFiltrado = new Set(filtrado);
+  return setFiltrado.size == 26;
+}
